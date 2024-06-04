3606c02cd5c967232de6f926b158594f0127e281
x507964 <x507964@LT132498>
2024-06-04 13:12:39 +0200

    Add project files.

---
+++ GroupFinance/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GroupFinance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
